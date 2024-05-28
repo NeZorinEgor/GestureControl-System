d6559db25d433772f154b901b1f81f025f674428
NeZorinEgor <zorin-ep@mail.ru>
2024-05-28 17:49:02 +0800

    refactor code

--- recognized/main.py
+++ recognized/main.py
@@ -2,20 +2,18 @@ import cv2
 import mediapipe as mp
 from datetime import datetime, timedelta
 
-# read web-camera / streem from ESP-32
+# read web-camera / stream from ESP-32
 camera = cv2.VideoCapture(0)
-# camera = cv2.VideoCapture("http://172.20.10.8:81/streem")
+# camera = cv2.VideoCapture("http://172.20.10.8:81/stream")
 
 # mediapipe hand settings
 mp_hand = mp.solutions.hands
 hands = mp_hand.Hands()
 mp_draw = mp.solutions.drawing_utils
 
-# list of poit id
-hand_points = [0 for point_id in range(21)]
-
-# list of fingertip
-up_fingers = [0 for finger in range(4)]
+hand_points = [0 for point_id in range(21)]        # list of point id
+up_fingers = [0 for finger in range(4)]            # list of fingertip
+finger_start_times = {i: None for i in range(4)}   # time when the finger was first detected up
 
 def is_up_finger(points, hand_center_id, fingertip_id, finger_in_list_id):
     """
@@ -35,10 +33,6 @@ def is_up_finger(points, hand_center_id, fingertip_id, finger_in_list_id):
         up_fingers[finger_in_list_id] = False
 
 
-# time when the finger was first detected up
-finger_start_times = {i: None for i in range(4)}
-
-
 while camera.isOpened():
     # Read frame
     _, frame = camera.read()
@@ -49,7 +43,7 @@ while camera.isOpened():
     if result.multi_hand_landmarks:
         for hand_lms in result.multi_hand_landmarks:
             mp_draw.draw_landmarks(frame, hand_lms, mp_hand.HAND_CONNECTIONS)
-            # Work with hand point. See https://www.researchgate.net/publication/357216549/figure/fig2/AS:1103448439304192@1640094017201/Hand-Landmark-in-MediaPipe-38.ppm
+            # Work with hand points
             for point_id, point_coordinates in enumerate(hand_lms.landmark):
                 width, height, color = frame.shape
                 width, height = int(point_coordinates.x * height), int(point_coordinates.y * width)
@@ -58,26 +52,29 @@ while camera.isOpened():
                 match point_id:
                     case 8: cv2.circle(frame, (width, height), 15, (0, 0, 255), cv2.FILLED)        # Index finger
                     case 12: cv2.circle(frame, (width, height), 15, (0, 255, 0), cv2.FILLED)       # Middle finger
-                    case 16: cv2.circle(frame, (width, height), 15, (255, 0, 0), cv2.FILLED)       # Ring finder
+                    case 16: cv2.circle(frame, (width, height), 15, (255, 0, 0), cv2.FILLED)       # Ring finger
                     case 20: cv2.circle(frame, (width, height), 15, (255, 255, 255), cv2.FILLED)   # Little finger
 
-            is_up_finger(hand_points, 5, 8, 0)
-            is_up_finger(hand_points, 9, 12, 1)
-            is_up_finger(hand_points, 13, 16, 2)
-            is_up_finger(hand_points, 17, 20, 3)
+            # Check up fingers
+            is_up_finger(hand_points, 5, 8, 0)     # Index finger
+            is_up_finger(hand_points, 9, 12, 1)    # Middle finger
+            is_up_finger(hand_points, 13, 16, 2)   # Ring finger
+            is_up_finger(hand_points, 17, 20, 3)   # Little finger
             # print(up_fingers)
 
+            # Check up finger for 3 seconds and do something...
             now = datetime.now()
             for i, finger_up in enumerate(up_fingers):
                 if finger_up:
                     if finger_start_times[i] is None:
                         finger_start_times[i] = now
                     elif now - finger_start_times[i] >= timedelta(seconds=3):
-                        match i:
-                            case 0: print("Index finger has been up for 3 seconds")
-                            case 1: print("Middle finger has been up for 3 seconds")
-                            case 2: print("Ring finger has been up for 3 seconds")
-                            case 3: print("Little finger has been up for 3 seconds")
+                        if up_fingers.count(True) == 1:
+                            match i:
+                                case 0: print("Index finger has been up for 3 seconds")
+                                case 1: print("Middle finger has been up for 3 seconds")
+                                case 2: print("Ring finger has been up for 3 seconds")
+                                case 3: print("Little finger has been up for 3 seconds")
                         # reset finger_start_time if needed
                         finger_start_times[i] = None
                 else:
--- recognized/test.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-
-start_time = datetime.now()
-
-while True:
-    now = datetime.now()
-    if now - start_time >= timedelta(seconds=3):
-        print(True)
-        break
