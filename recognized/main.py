ecfeb2a6f592dfae2935da118363907d501244a3
NeZorinEgor <zorin-ep@mail.ru>
2024-05-29 19:30:13 +0800

    feat: add third mode

--- recognized/main.py
+++ recognized/main.py
@@ -14,29 +14,31 @@ finger_start_times = {i: None for i in range(4)}   # time when the finger was fi
 all_fingers_bent_time = None                       # time when all fingers were first detected bent
 distance_mode = False                              # flag for distance measurement mode
 print_fingers_mode = False                         # flag for print fingers mode
+print_coordinate_mode = False                      # flag for print coordinate mode
 previous_distance = None                           # Initialize previous distance as None
+
 
 def calculate_distance_between_finger(x1, y1, x2, y2):
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
+
 def get_coordinate(hand_landmark, index, image):
     return int(hand_landmark.landmark[index].x * image.shape[1]), int(hand_landmark.landmark[index].y * image.shape[0])
 
+
 def process_hand_landmarks(image, hand_landmarks):
     mp_draw.draw_landmarks(image, hand_landmarks, mp_hand.HAND_CONNECTIONS)
-
     x1, y1 = get_coordinate(hand_landmarks, 8, image)  # Index finger tip
     x2, y2 = get_coordinate(hand_landmarks, 4, image)  # Thumb tip
-
     distance = calculate_distance_between_finger(x1, y1, x2, y2)
     distance_str = "{:.2f}".format(distance)
-
     cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
     cv2.circle(image, (x1, y1), 10, (255, 0, 0), -1)
     cv2.circle(image, (x2, y2), 10, (255, 0, 0), -1)
     cv2.putText(image, distance_str, (int((x1 + x2) / 2), int((y1 + y2) / 2) - 10),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     return distance
+
 
 def is_up_finger(points, hand_center_id, fingertip_id, finger_in_list_id):
     """
@@ -55,83 +57,106 @@ def is_up_finger(points, hand_center_id, fingertip_id, finger_in_list_id):
     else:
         up_fingers[finger_in_list_id] = False
 
-def main():
-    global previous_distance, distance_mode, all_fingers_bent_time, print_fingers_mode
+# read web-camera / stream from ESP-32
+camera = cv2.VideoCapture(0)
+# camera = cv2.VideoCapture("http://172.20.10.8:81/stream")
 
-    camera = cv2.VideoCapture(0)
-    while camera.isOpened():
-        _, frame = camera.read()
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+while camera.isOpened():
+    # Read frame
+    _, frame = camera.read()
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        result = hands.process(frame_rgb)
-        if result.multi_hand_landmarks:
-            for hand_lms in result.multi_hand_landmarks:
-                mp_draw.draw_landmarks(frame, hand_lms, mp_hand.HAND_CONNECTIONS)
-                for point_id, point_coordinates in enumerate(hand_lms.landmark):
-                    width, height, color = frame.shape
-                    width, height = int(point_coordinates.x * height), int(point_coordinates.y * width)
-                    hand_points[point_id] = height
-                    match point_id:
-                        case 8: cv2.circle(frame, (width, height), 15, (0, 0, 255), cv2.FILLED)        # Index finger
-                        case 12: cv2.circle(frame, (width, height), 15, (0, 255, 0), cv2.FILLED)       # Middle finger
-                        case 16: cv2.circle(frame, (width, height), 15, (255, 0, 0), cv2.FILLED)       # Ring finger
-                        case 20: cv2.circle(frame, (width, height), 15, (255, 255, 255), cv2.FILLED)   # Little finger
+    # Draw hand
+    result = hands.process(frame_rgb)
+    if result.multi_hand_landmarks:
+        for hand_lms in result.multi_hand_landmarks:
+            mp_draw.draw_landmarks(frame, hand_lms, mp_hand.HAND_CONNECTIONS)
 
-                is_up_finger(hand_points, 5, 8, 0)     # Index finger
-                is_up_finger(hand_points, 9, 12, 1)    # Middle finger
-                is_up_finger(hand_points, 13, 16, 2)   # Ring finger
-                is_up_finger(hand_points, 17, 20, 3)   # Little finger
+            # Work with hand points
+            for point_id, point_coordinates in enumerate(hand_lms.landmark):
+                width, height, color = frame.shape
+                width, height = int(point_coordinates.x * height), int(point_coordinates.y * width)
+                hand_points[point_id] = height
 
-                now = datetime.now()
-                for i, finger_up in enumerate(up_fingers):
-                    if finger_up:
-                        if finger_start_times[i] is None:
-                            finger_start_times[i] = now
-                        elif now - finger_start_times[i] >= timedelta(seconds=3):
-                            if up_fingers.count(True) == 1:
-                                if i == 0:
-                                    distance_mode = True
-                                    print_fingers_mode = False
-                                    print("Index finger has been up for 3 seconds, entering distance measurement mode")
-                                elif i == 3:
-                                    print_fingers_mode = True
-                                    distance_mode = False
-                                    print("Little finger has been up for 3 seconds, entering print fingers mode")
-                                    print(up_fingers)
-                            finger_start_times[i] = None
-                    else:
+                # Draw color point at frame
+                match point_id:
+                    case 8: cv2.circle(frame, (width, height), 15, (0, 0, 255), cv2.FILLED)        # Index finger
+                    case 12: cv2.circle(frame, (width, height), 15, (0, 255, 0), cv2.FILLED)       # Middle finger
+                    case 16: cv2.circle(frame, (width, height), 15, (255, 0, 0), cv2.FILLED)       # Ring finger
+                    case 20: cv2.circle(frame, (width, height), 15, (255, 255, 255), cv2.FILLED)   # Little finger
+
+            # Check up fingers
+            is_up_finger(hand_points, 5, 8, 0)     # Index finger
+            is_up_finger(hand_points, 9, 12, 1)    # Middle finger
+            is_up_finger(hand_points, 13, 16, 2)   # Ring finger
+            is_up_finger(hand_points, 17, 20, 3)   # Little finger
+
+            # Check finger uptime and set mode
+            now = datetime.now()
+            for i, finger_up in enumerate(up_fingers):
+                if finger_up:
+                    if finger_start_times[i] is None:
+                        finger_start_times[i] = now
+                    elif now - finger_start_times[i] >= timedelta(seconds=3):
+                        if up_fingers.count(True) == 1:
+                            if i == 0:
+                                distance_mode = True
+                                print_fingers_mode = False
+                                print_coordinate_mode = False
+                                print("Index finger has been up for 3 seconds, entering distance measurement mode")
+                            elif i == 3:
+                                print_fingers_mode = True
+                                distance_mode = False
+                                print_coordinate_mode = False
+                                print("Little finger has been up for 3 seconds, entering print fingers mode")
+                                print(up_fingers)
+                            elif i == 1:
+                                print_coordinate_mode = True
+                                print_fingers_mode = False
+                                distance_mode = False
+                                print("Middle finger has been up for 3 seconds, entering print index finger coordinates mode")
+
                         finger_start_times[i] = None
-
-                if up_fingers.count(True) == 0:
-                    if all_fingers_bent_time is None:
-                        all_fingers_bent_time = now
-                    elif now - all_fingers_bent_time >= timedelta(seconds=3):
-                        distance_mode = False
-                        print_fingers_mode = False
-                        print("All fingers have been bent for 3 seconds, exiting all modes")
-                        all_fingers_bent_time = None
                 else:
+                    finger_start_times[i] = None
+
+            if up_fingers.count(True) == 0:
+                if all_fingers_bent_time is None:
+                    all_fingers_bent_time = now
+                elif now - all_fingers_bent_time >= timedelta(seconds=3):
+                    distance_mode = False
+                    print_fingers_mode = False
+                    print_coordinate_mode = False
+                    print("All fingers have been bent for 3 seconds, exiting all modes")
                     all_fingers_bent_time = None
+            else:
+                all_fingers_bent_time = None
 
-                if distance_mode:
-                    current_distance = process_hand_landmarks(frame, hand_lms)
-                    if previous_distance is not None:
-                        if current_distance > previous_distance:
-                            print(True)
-                        else:
-                            print(False)
-                    previous_distance = current_distance
+            # Set mod`s
+            if distance_mode and print_coordinate_mode != True:
+                current_distance = process_hand_landmarks(frame, hand_lms)
+                if previous_distance is not None:
+                    if current_distance > previous_distance:
+                        print(True)
+                    else:
+                        print(False)
+                previous_distance = current_distance
 
-                if print_fingers_mode:
-                    print("Printing up_fingers:", up_fingers)
+            if print_fingers_mode:
+                print("Printing up_fingers:", up_fingers)
 
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) == ord('q'):
-            break
+            if print_coordinate_mode:
+                distance_mode = False
+                finger_start_times[0] = None
+                print(hand_lms.landmark[8])
 
-    camera.release()
-    cv2.destroyAllWindows()
+    # Draw image
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Close window
+camera.release()
+cv2.destroyAllWindows()
 
 
-if __name__ == '__main__':
-    main()
