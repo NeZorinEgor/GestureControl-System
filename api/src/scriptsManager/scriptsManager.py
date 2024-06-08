1c5d1f3c719d8df9847c553a7d447c1efc298da8
nech14 <nech14@mail.ru>
2024-06-08 09:46:16 +0800

    Create scriptManager

---
+++ api/src/scriptsManager/scriptsManager.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+import subprocess
+
+save_dir = "../../../scripts"
+
+
+def startScript(path, mac="", force=100):
+    # Параметры, которые нужно передать
+    params = [str(mac), str(force)]
+
+    # Запуск файла с параметрами
+    result = subprocess.run(['python', path] + params, capture_output=True, text=True)
+    return result.stdout
+
+
+def saveScript(path, name=None, save_dir=None):
+
+    if save_dir is None:
+        save_dir = ''
+
+    if name is None:
+        name = os.path.basename(path)
+
+    # Полный путь к новому файлу
+    new_file_path = os.path.join(save_dir, name)
+
+    # Создаем новую папку, если она не существует
+    os.makedirs(save_dir, exist_ok=True)
+
+    # Перемещаем и переименовываем файл
+    shutil.copy2(path, new_file_path)
+
+    return new_file_path
+
+
+def deleteScript(path):
+    if os.path.exists(path):
+        os.remove(path)
+        return True
+    else:
+        return False
